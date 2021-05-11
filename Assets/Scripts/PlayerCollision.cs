7103ca7576e7d310dde55f0b051e5334897c429b
Kenny Nguyen <72421222+itsmeKenny@users.noreply.github.com>
2021-05-11 11:27:42 -0700

    Test merge
    Added background and finished track

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerMovement movement;
+    private int count;
+    public TextMeshProUGUI countText;
+
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+    }
 
     void OnCollisionEnter(Collision collisionInfo)
     {
@@ -14,5 +23,20 @@ public class PlayerCollision : MonoBehaviour
             
             //FindObjectOfType<GameManager>().EndGame(); //Looks for gamemanager object and calls that
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp")) 
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
     }
 }
