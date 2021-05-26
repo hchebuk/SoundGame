eb3bdd45aaa39634d5950b6a64b51ab682d9507d
Harrison Chebuk <31672501+hchebuk@users.noreply.github.com>
2021-05-26 11:30:06 -0700

    Last minute bugs

--- Assets/Scripts/HardLevel/MainMenu.cs
+++ Assets/Scripts/HardLevel/MainMenu.cs
@@ -1,15 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
 public class MainMenu : MonoBehaviour
-{
-  public void PlayTheGame()
-  {
-    SceneManager.LoadScene("Level 1");
-  }
-
+{ 
   public void QuitTheGame()
     {
     Debug.Log("QUIT THE GAME");
